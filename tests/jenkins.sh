d1bde7fd0f0224eaa378832ae76fe2511e5848ed
Stephan <stephan@scheller.com>
2012-06-03 20:54:38 -0700

    allow jenkins.sh test script to run without jenkins

--- tests/jenkins.sh
+++ tests/jenkins.sh
@@ -1,3 +1,16 @@
+# if workspace does not exist, create in root of project dir
+if [ -z $WORKSPACE ]; then
+    DIR="$( cd "$( dirname "$0" )"/.. && pwd )"
+    WORKSPACE=$DIR/workspace
+    if [ ! -d $WORKSPACE ]; then
+        echo "workspace dir does not exist, creating"
+        mkdir $WORKSPACE
+        if [ $? -ne 0 ]; then
+            exit 1
+        fi
+    fi
+fi
+
 PYENV_HOME=$WORKSPACE/.pyenv/
 
 # Delete previously built virtualenv
